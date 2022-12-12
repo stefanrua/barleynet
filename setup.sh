32c1f7dc0739c6d806d46dd919284cf9f6443a3c
Stefan Rua <stefan.rua@iki.fi>
2022-12-12 15:15:30 +0200

    Fix typo

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ git submodule update
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install torch torchvision
-python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
+python3 -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 mkdir models images tiles
 wget -P models/ https://a3s.fi/swift/v1/AUTH_4b8657d0f1c44e83bae2cd293af06d2a/barleynet-models/faster-rcnn/model_0061999.pth
