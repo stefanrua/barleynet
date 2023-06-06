8eb4320967a5435df8e2f899507a396df2ab8a82
Stefan Rua <stefan.rua@iki.fi>
2023-06-06 14:29:48 +0300

    Add some nice options to draw.py

--- draw.py
+++ draw.py
@@ -11,23 +11,57 @@ predfile = 'predictions.json'
 with open(predfile, 'r') as f:
     predictions = json.load(f)
 
-def draw(fname):
+def draw(fname, score, args):
     im = Image.open(f'{imgdir}{fname}')
     fig, ax = plt.subplots()
     ax.imshow(im)
+    print(fname)
+    print(score)
+    print(args)
     for pred in predictions[fname]:
-        if pred['score'] >= 0.5:
+        if pred['score'] >= score:
             bbox = pred['bbox']
-            rect = patches.Rectangle(
-                    (bbox[0], bbox[1]), bbox[2], bbox[3],
-                    linewidth=1,
-                    edgecolor='white',
-                    facecolor='none')
-            ax.add_patch(rect)
+            alpha = 1
+            if 'alpha' in args:
+                alpha = pred['score']
+            if 'box' in args:
+                rect = patches.Rectangle(
+                        (bbox[0], bbox[1]), bbox[2], bbox[3],
+                        linewidth=2,
+                        edgecolor='white',
+                        facecolor='none',
+                        alpha=alpha)
+                ax.add_patch(rect)
+            if 'dot' in args:
+                ax.plot(bbox[0]+bbox[2]/2,
+                        bbox[1]+bbox[3]/2,
+                        marker='.',
+                        color='white',
+                        alpha=alpha)
+            if 'text' in args:
+                ax.text(bbox[0],
+                        bbox[1],
+                        str(round(pred['score'], 2)),
+                        color='white',
+                        alpha=alpha)
     plt.show()
 
-if len(sys.argv) > 1:
-    draw(sys.argv[1])
-else:
-    for fname in os.listdir(imgdir):
-        draw(fname)
+#if len(sys.argv) > 1:
+#    draw(sys.argv[1])
+#else:
+#    for fname in os.listdir(imgdir):
+#        draw(fname)
+
+fname = ''
+score = 0.5
+args = ['box', 'alpha']
+while True:
+    try:
+        f = input('Filename: ')
+        if len(f) > 0: fname = f
+        s = input('Score: ')
+        if len(s) > 0: score = float(s)
+        a = input('Args: ')
+        if len(a) > 0: args = a.split(' ')
+        draw(fname, score, args)
+    except Exception as e: print(e)
